056e711e86772527f89ab75818c38cd60eb16483
Axel Barrios <axel.barrios@gmail.com>
2025-05-26 08:30:28 -0600

    Updated FormHeader.tsx

--- src/components/FormHeader.tsx
+++ src/components/FormHeader.tsx
@@ -50,14 +50,7 @@ const FormHeader: React.FC<FormHeaderProps> = ({ spouse, onToggleSpouse, showIns
           {spouse === 'husband' ? t('husbandForm') : t('wifeForm')}
         </h2>
         
-        <div className="flex justify-center">
-        <button>
-           // onClick={onToggleSpouse}
-           // className="text-indigo-300 hover:text-indigo-200 underline transition-colors"
-          //>
-           // {spouse === 'husband' ? t('switchToWife') : t('switchToHusband')}>
-         // </button>
-        //</div>
+        
       </div>
       
       {showInstructions && (
